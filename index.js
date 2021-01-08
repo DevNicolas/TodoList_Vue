20aec6b57a486560680bc6272d329c2c8f1b61c7
DevNicolas <skool980825@gmail.com>
2021-01-07 22:18:45 -0500

    Agregando Cache
    Se agrega LocalStorage para guardar los datos en cache

--- index.js
+++ index.js
@@ -12,9 +12,23 @@ const app = new Vue({
         estado: false,
       });
       this.nuevaTarea = "";
+      localStorage.setItem("todo-list", JSON.stringify(this.tareas));
     },
     completarTarea(index) {
       this.tareas[index].estado = true;
+      localStorage.setItem("todo-list", JSON.stringify(this.tareas));
     },
+    eliminarTarea(index) {
+      this.tareas.splice(index, 1);
+      localStorage.setItem("todo-list", JSON.stringify(this.tareas));
+    },
+  },
+  created: function () {
+    let datosCache = JSON.parse(localStorage.getItem("todo-list"));
+    if (!datosCache) {
+      this.tareas = [];
+    } else {
+      this.tareas = datosCache;
+    }
   },
 });
